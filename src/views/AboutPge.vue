2a4831d517da5a22ecf80e1e8f71b2b34e4993fd
NuraNet <90922250+NuraNet@users.noreply.github.com>
2022-05-07 22:13:33 -0400

    Added About page some placeholder content.

--- src/views/AboutPge.vue
+++ src/views/AboutPge.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <p>Working on this...<br><br>
+    Feel free to reach out to me on <a :href="linkedURL"
+    target="_new">
+    LinkedIn.</a></p>
   </div>
 </template>
+
+<script setup>
+const linkedURL = 'https://www.linkedin.com/in/linkarungeorge/'
+</script>
